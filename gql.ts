bef9d02b52a2dffb5361623b712f31ab39927912
McCall Alexander <mccallalexander96@gmail.com>
2020-12-18 14:17:30 -0700

    Update gql.ts

--- gql.ts
+++ gql.ts
@@ -1,30 +1,32 @@
-export const gql = (...templateLiteral: Parameters<typeof String.raw>) => {
-  const query = String.raw(...templateLiteral);
-  let variables;
-  const request = () =>
-    fetch('/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        query,
-        ...(variables ? { variables } : {}),
-      }),
-    })
-      .then((r) => r.json())
-      .then((r) => r.data);
-  return createInvocablePromise(
-    (vars) => {
-      variables = vars;
-      return request();
-    },
-    (resolve, reject) => {
-      const timeout = setTimeout(() => {
-        if (variables) return resolve(true);
-        return resolve(request());
-      }, 0);
-    }
-  );
-};
+export const gql = createTemplateFunction((query) => {
+	let variables;
+	const request = (): Promise<GqlTypes.Query> =>
+		fetch(
+			'/graphql',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify({
+					query,
+					...(variables ? { variables } : {}),
+				}),
+			}
+		)
+			.then((r) => r.json())
+			.then((r) => r.data);
+	return createInvocablePromise<GqlTypes.Query>(
+		(vars) => {
+			variables = vars;
+			return request();
+		},
+		(resolve, reject) => {
+			const timeout = setTimeout(() => {
+				if (variables) return resolve(true);
+				return resolve(request());
+			}, 0);
+		}
+	);
+});
